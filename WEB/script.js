ef26b0bf9e785c3b17f7413c92776b2799d354e2
josemSVQ <131583418+josemSVQ@users.noreply.github.com>
2023-04-23 10:41:25 +0200

    Add files via upload

---
+++ WEB/script.js
@@ -0,0 +1,5 @@
+// Script de JavaScript para el sitio web de gestores de contenidos
+
+// Comentario: Apartado 1b - Se incluye un comentario que indica el apartado numerico y la palabra marcada en negrita en los elementos de la lista de caracteristicas
+document.getElementById('caracteristicas-1').innerHTML += ' (Apartado 1b - Facilidad de uso)';
+document.getElementById('caracteristicas-2').innerHTML += ' (Apartado 1b - Flexibilidad y personalizacion)';
